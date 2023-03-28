69349a18d77fb1d730d8ea192ddaac810d4a1ad5
serkan <serkan.yalcin@booklogic.net>
2023-03-28 21:51:35 +0300

    Eğer tarih, sistem saatine eşitse, o günü daha belirgin hale getiriyoruz.

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const ilSelect = document.getElementById('il-select');
-  const ilceSelect = document.getElementById('ilce-select');
-  const ilUrl = 'https://ezanvakti.herokuapp.com/sehirler/2';
+document.addEventListener("DOMContentLoaded", function () {
+  const ilSelect = document.getElementById("il-select");
+  const ilceSelect = document.getElementById("ilce-select");
+  const ilUrl = "https://ezanvakti.herokuapp.com/sehirler/2";
 
   fetch(ilUrl)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       // Şehir seçeneklerini seçim alanına ekliyoruz
-      data.forEach(sehir => {
-        const option = document.createElement('option');
+      data.forEach((sehir) => {
+        const option = document.createElement("option");
         option.value = sehir.SehirID;
         option.text = sehir.SehirAdi;
         ilSelect.appendChild(option);
-
-
       });
     })
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 
   // İl seçimi değiştiğinde ilçe seçeneklerini getiriyoruz
-  ilSelect.addEventListener('change', () => {
+  ilSelect.addEventListener("change", () => {
     const ilceUrl = `https://ezanvakti.herokuapp.com/ilceler/${ilSelect.value}`;
 
     // İlçe seçim alanını boşaltıyoruz
-    ilceSelect.innerHTML = '<option selected>Lütfen önce bir il seçin...</option>';
+    ilceSelect.innerHTML =
+      "<option selected>Lütfen önce bir il seçin...</option>";
     ilceSelect.disabled = true;
 
     console.log(ilSelect.selected);
 
-
     // İl seçiliyse, ilçeleri getir
-    if (ilSelect.value !== '') {
-
+    if (ilSelect.value !== "") {
       // İlçe seçeneklerini API'den getiriyoruz
       fetch(ilceUrl)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           // İlçe seçeneklerini seçim alanına ekliyoruz
           if (Array.isArray(data)) {
-            data.forEach(ilce => {
-              const option = document.createElement('option');
+            data.forEach((ilce) => {
+              const option = document.createElement("option");
               option.value = ilce.IlceID;
               option.text = ilce.IlceAdi;
               ilceSelect.appendChild(option);
@@ -49,38 +46,41 @@ document.addEventListener("DOMContentLoaded", function() {
           // İlçe seçim alanını aktif hale getiriyoruz
           ilceSelect.disabled = false;
         })
-        .catch(error => console.error(error));
+        .catch((error) => console.error(error));
     }
   });
 
+  // Bugünün tarihini alıyoruz
+  const today = new Date();
+  const todayString = today.toLocaleDateString();
 
   // Namaz saatlerini getiriyoruz
-  ilceSelect.addEventListener('change', () => {
+  ilceSelect.addEventListener("change", () => {
     const namazUrl = `https://ezanvakti.herokuapp.com/vakitler?ilce=${ilceSelect.value}`;
 
     fetch(namazUrl)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         // Namaz saatlerini gösteriyoruz
-        const table = document.createElement('table');
-        table.classList.add('table', 'table-striped', 'table-hover');
-        const thead = document.createElement('thead');
-        const tbody = document.createElement('tbody');
-        const trHead = document.createElement('tr');
-        const thTarih = document.createElement('th');
-        const thImsak = document.createElement('th');
-        const thGunes = document.createElement('th');
-        const thOgle = document.createElement('th');
-        const thIkindi = document.createElement('th');
-        const thAksam = document.createElement('th');
-        const thYatsi = document.createElement('th');
-        thTarih.textContent = 'Tarih';
-        thImsak.textContent = 'İmsak';
-        thGunes.textContent = 'Güneş';
-        thOgle.textContent = 'Öğle';
-        thIkindi.textContent = 'İkindi';
-        thAksam.textContent = 'Akşam';
-        thYatsi.textContent = 'Yatsı';
+        const table = document.createElement("table");
+        table.classList.add("table", "table-striped", "table-hover");
+        const thead = document.createElement("thead");
+        const tbody = document.createElement("tbody");
+        const trHead = document.createElement("tr");
+        const thTarih = document.createElement("th");
+        const thImsak = document.createElement("th");
+        const thGunes = document.createElement("th");
+        const thOgle = document.createElement("th");
+        const thIkindi = document.createElement("th");
+        const thAksam = document.createElement("th");
+        const thYatsi = document.createElement("th");
+        thTarih.textContent = "Tarih";
+        thImsak.textContent = "İmsak";
+        thGunes.textContent = "Güneş";
+        thOgle.textContent = "Öğle";
+        thIkindi.textContent = "İkindi";
+        thAksam.textContent = "Akşam";
+        thYatsi.textContent = "Yatsı";
         trHead.appendChild(thTarih);
         trHead.appendChild(thImsak);
         trHead.appendChild(thGunes);
@@ -90,17 +90,26 @@ document.addEventListener("DOMContentLoaded", function() {
         trHead.appendChild(thYatsi);
         thead.appendChild(trHead);
         table.appendChild(thead);
-    
-        data.forEach(vakit => {
-          const trBody = document.createElement('tr');
-          const tdTarih = document.createElement('td');
-          const tdImsak = document.createElement('td');
-          const tdGunes = document.createElement('td');
-          const tdOgle = document.createElement('td');
-          const tdIkindi = document.createElement('td');
-          const tdAksam = document.createElement('td');
-          const tdYatsi = document.createElement('td');
-          tdTarih.textContent = new Date(vakit.MiladiTarihUzunIso8601).toLocaleDateString();
+
+        data.forEach((vakit) => {
+          const trBody = document.createElement("tr");
+          const tdTarih = document.createElement("td");
+          const tdImsak = document.createElement("td");
+          const tdGunes = document.createElement("td");
+          const tdOgle = document.createElement("td");
+          const tdIkindi = document.createElement("td");
+          const tdAksam = document.createElement("td");
+          const tdYatsi = document.createElement("td");
+          tdTarih.textContent = new Date(
+            vakit.MiladiTarihUzunIso8601
+          ).toLocaleDateString();
+
+          // Eğer tarih, sistem saatine eşitse, o günü daha belirgin hale getiriyoruz
+          if (tdTarih.textContent === todayString) {
+            tdTarih.style.fontWeight = "bold";
+            tdTarih.style.color = "red";
+          }
+
           tdImsak.textContent = vakit.Imsak;
           tdGunes.textContent = vakit.Gunes;
           tdOgle.textContent = vakit.Ogle;
@@ -117,11 +126,11 @@ document.addEventListener("DOMContentLoaded", function() {
           tbody.appendChild(trBody);
         });
         table.appendChild(tbody);
-    
-        const namazSaatleriDiv = document.getElementById('namaz-saatleri');
-        namazSaatleriDiv.innerHTML = '';
+
+        const namazSaatleriDiv = document.getElementById("namaz-saatleri");
+        namazSaatleriDiv.innerHTML = "";
         namazSaatleriDiv.appendChild(table);
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
   });
 });
